df27e4ce71657f528d4e1acae520d2d9d6a2db52
Benjamin Zaporzan <benzaporzan@gmail.com>
2016-04-02 16:55:57 -0400

    updated prod

---
+++ js/compiled/out/redditv/icons.js
@@ -0,0 +1,7 @@
+// Compiled by ClojureScript 1.7.228 {}
+goog.provide('redditv.icons');
+goog.require('cljs.core');
+goog.require('om.dom');
+redditv.icons.google_icon = (function redditv$icons$google_icon(icon_name){
+return React.DOM.i({"className": "material-icons noselect"},om.util.force_children.call(null,icon_name));
+});
