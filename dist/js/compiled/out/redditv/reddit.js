b9f8ce99129690846ae7a544c2ebdc1721d2010f
Benjamin Zaporzan <benzaporzan@gmail.com>
2016-04-04 18:35:18 -0400

    updated prod

--- dist/js/compiled/out/redditv/reddit.js
+++ dist/js/compiled/out/redditv/reddit.js
@@ -4,115 +4,115 @@ goog.require('cljs.core');
 goog.require('cljs.core.async');
 goog.require('redditv.jsonp');
 goog.require('redditv.youtube');
-redditv.reddit.reddit_url = "http://www.reddit.com";
+redditv.reddit.reddit_url = "https://www.reddit.com";
 redditv.reddit.get_subreddit_posts = (function redditv$reddit$get_subreddit_posts(subreddit){
 var output_channel = cljs.core.async.chan.call(null);
 var url = [cljs.core.str(redditv.reddit.reddit_url),cljs.core.str("/r/"),cljs.core.str(subreddit),cljs.core.str(".json?limit=100")].join('');
-var vec__32744 = redditv.jsonp.send_jsonp.call(null,url);
-var success_channel = cljs.core.nth.call(null,vec__32744,(0),null);
-var error_channel = cljs.core.nth.call(null,vec__32744,(1),null);
-var c__29924__auto___32764 = cljs.core.async.chan.call(null,(1));
-cljs.core.async.impl.dispatch.run.call(null,((function (c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel){
+var vec__27052 = redditv.jsonp.send_jsonp.call(null,url);
+var success_channel = cljs.core.nth.call(null,vec__27052,(0),null);
+var error_channel = cljs.core.nth.call(null,vec__27052,(1),null);
+var c__24744__auto___27072 = cljs.core.async.chan.call(null,(1));
+cljs.core.async.impl.dispatch.run.call(null,((function (c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel){
 return (function (){
-var f__29925__auto__ = (function (){var switch__29812__auto__ = ((function (c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel){
-return (function (state_32755){
-var state_val_32756 = (state_32755[(1)]);
-if((state_val_32756 === (1))){
-var state_32755__$1 = state_32755;
-return cljs.core.async.impl.ioc_helpers.take_BANG_.call(null,state_32755__$1,(2),success_channel);
+var f__24745__auto__ = (function (){var switch__24679__auto__ = ((function (c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel){
+return (function (state_27063){
+var state_val_27064 = (state_27063[(1)]);
+if((state_val_27064 === (1))){
+var state_27063__$1 = state_27063;
+return cljs.core.async.impl.ioc_helpers.take_BANG_.call(null,state_27063__$1,(2),success_channel);
 } else {
-if((state_val_32756 === (2))){
-var inst_32746 = (state_32755[(2)]);
-var inst_32747 = cljs.core.js__GT_clj.call(null,inst_32746,new cljs.core.Keyword(null,"keywordize-keys","keywordize-keys",1310784252),true);
-var inst_32748 = new cljs.core.Keyword(null,"data","data",-232669377).cljs$core$IFn$_invoke$arity$1(inst_32747);
-var inst_32749 = new cljs.core.Keyword(null,"children","children",-940561982).cljs$core$IFn$_invoke$arity$1(inst_32748);
-var inst_32750 = cljs.core.vec.call(null,inst_32749);
-var inst_32751 = (function (){var result = inst_32747;
-var data = inst_32750;
-return ((function (result,data,inst_32746,inst_32747,inst_32748,inst_32749,inst_32750,state_val_32756,c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel){
-return (function (p1__32723_SHARP_){
-return new cljs.core.Keyword(null,"data","data",-232669377).cljs$core$IFn$_invoke$arity$1(p1__32723_SHARP_);
+if((state_val_27064 === (2))){
+var inst_27054 = (state_27063[(2)]);
+var inst_27055 = cljs.core.js__GT_clj.call(null,inst_27054,new cljs.core.Keyword(null,"keywordize-keys","keywordize-keys",1310784252),true);
+var inst_27056 = new cljs.core.Keyword(null,"data","data",-232669377).cljs$core$IFn$_invoke$arity$1(inst_27055);
+var inst_27057 = new cljs.core.Keyword(null,"children","children",-940561982).cljs$core$IFn$_invoke$arity$1(inst_27056);
+var inst_27058 = cljs.core.vec.call(null,inst_27057);
+var inst_27059 = (function (){var result = inst_27055;
+var data = inst_27058;
+return ((function (result,data,inst_27054,inst_27055,inst_27056,inst_27057,inst_27058,state_val_27064,c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel){
+return (function (p1__27031_SHARP_){
+return new cljs.core.Keyword(null,"data","data",-232669377).cljs$core$IFn$_invoke$arity$1(p1__27031_SHARP_);
 });
-;})(result,data,inst_32746,inst_32747,inst_32748,inst_32749,inst_32750,state_val_32756,c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel))
+;})(result,data,inst_27054,inst_27055,inst_27056,inst_27057,inst_27058,state_val_27064,c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel))
 })();
-var inst_32752 = cljs.core.map.call(null,inst_32751,inst_32750);
-var inst_32753 = cljs.core.async.put_BANG_.call(null,output_channel,inst_32752);
-var state_32755__$1 = state_32755;
-return cljs.core.async.impl.ioc_helpers.return_chan.call(null,state_32755__$1,inst_32753);
+var inst_27060 = cljs.core.map.call(null,inst_27059,inst_27058);
+var inst_27061 = cljs.core.async.put_BANG_.call(null,output_channel,inst_27060);
+var state_27063__$1 = state_27063;
+return cljs.core.async.impl.ioc_helpers.return_chan.call(null,state_27063__$1,inst_27061);
 } else {
 return null;
 }
 }
-});})(c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel))
+});})(c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel))
 ;
-return ((function (switch__29812__auto__,c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel){
+return ((function (switch__24679__auto__,c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel){
 return (function() {
-var redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto__ = null;
-var redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto____0 = (function (){
-var statearr_32760 = [null,null,null,null,null,null,null];
-(statearr_32760[(0)] = redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto__);
+var redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto__ = null;
+var redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto____0 = (function (){
+var statearr_27068 = [null,null,null,null,null,null,null];
+(statearr_27068[(0)] = redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto__);
 
-(statearr_32760[(1)] = (1));
+(statearr_27068[(1)] = (1));
 
-return statearr_32760;
+return statearr_27068;
 });
-var redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto____1 = (function (state_32755){
+var redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto____1 = (function (state_27063){
 while(true){
-var ret_value__29814__auto__ = (function (){try{while(true){
-var result__29815__auto__ = switch__29812__auto__.call(null,state_32755);
-if(cljs.core.keyword_identical_QMARK_.call(null,result__29815__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
+var ret_value__24681__auto__ = (function (){try{while(true){
+var result__24682__auto__ = switch__24679__auto__.call(null,state_27063);
+if(cljs.core.keyword_identical_QMARK_.call(null,result__24682__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
 continue;
 } else {
-return result__29815__auto__;
+return result__24682__auto__;
 }
 break;
 }
-}catch (e32761){if((e32761 instanceof Object)){
-var ex__29816__auto__ = e32761;
-var statearr_32762_32765 = state_32755;
-(statearr_32762_32765[(5)] = ex__29816__auto__);
+}catch (e27069){if((e27069 instanceof Object)){
+var ex__24683__auto__ = e27069;
+var statearr_27070_27073 = state_27063;
+(statearr_27070_27073[(5)] = ex__24683__auto__);
 
 
-cljs.core.async.impl.ioc_helpers.process_exception.call(null,state_32755);
+cljs.core.async.impl.ioc_helpers.process_exception.call(null,state_27063);
 
 return new cljs.core.Keyword(null,"recur","recur",-437573268);
 } else {
-throw e32761;
+throw e27069;
 
 }
 }})();
-if(cljs.core.keyword_identical_QMARK_.call(null,ret_value__29814__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
-var G__32766 = state_32755;
-state_32755 = G__32766;
+if(cljs.core.keyword_identical_QMARK_.call(null,ret_value__24681__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
+var G__27074 = state_27063;
+state_27063 = G__27074;
 continue;
 } else {
-return ret_value__29814__auto__;
+return ret_value__24681__auto__;
 }
 break;
 }
 });
-redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto__ = function(state_32755){
+redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto__ = function(state_27063){
 switch(arguments.length){
 case 0:
-return redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto____0.call(this);
+return redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto____0.call(this);
 case 1:
-return redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto____1.call(this,state_32755);
+return redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto____1.call(this,state_27063);
 }
 throw(new Error('Invalid arity: ' + arguments.length));
 };
-redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto__.cljs$core$IFn$_invoke$arity$0 = redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto____0;
-redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto__.cljs$core$IFn$_invoke$arity$1 = redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto____1;
-return redditv$reddit$get_subreddit_posts_$_state_machine__29813__auto__;
+redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto__.cljs$core$IFn$_invoke$arity$0 = redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto____0;
+redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto__.cljs$core$IFn$_invoke$arity$1 = redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto____1;
+return redditv$reddit$get_subreddit_posts_$_state_machine__24680__auto__;
 })()
-;})(switch__29812__auto__,c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel))
+;})(switch__24679__auto__,c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel))
 })();
-var state__29926__auto__ = (function (){var statearr_32763 = f__29925__auto__.call(null);
-(statearr_32763[cljs.core.async.impl.ioc_helpers.USER_START_IDX] = c__29924__auto___32764);
+var state__24746__auto__ = (function (){var statearr_27071 = f__24745__auto__.call(null);
+(statearr_27071[cljs.core.async.impl.ioc_helpers.USER_START_IDX] = c__24744__auto___27072);
 
-return statearr_32763;
+return statearr_27071;
 })();
-return cljs.core.async.impl.ioc_helpers.run_state_machine_wrapped.call(null,state__29926__auto__);
-});})(c__29924__auto___32764,output_channel,url,vec__32744,success_channel,error_channel))
+return cljs.core.async.impl.ioc_helpers.run_state_machine_wrapped.call(null,state__24746__auto__);
+});})(c__24744__auto___27072,output_channel,url,vec__27052,success_channel,error_channel))
 );
 
 
@@ -123,99 +123,99 @@ return redditv.youtube.is_youtube_url_QMARK_.call(null,post.call(null,new cljs.c
 });
 redditv.reddit.get_subreddit_videos = (function redditv$reddit$get_subreddit_videos(subreddit){
 var output_channel = cljs.core.async.chan.call(null);
-var vec__32782 = redditv.reddit.get_subreddit_posts.call(null,subreddit);
-var success_channel = cljs.core.nth.call(null,vec__32782,(0),null);
-var error_channel = cljs.core.nth.call(null,vec__32782,(1),null);
-var c__29924__auto___32797 = cljs.core.async.chan.call(null,(1));
-cljs.core.async.impl.dispatch.run.call(null,((function (c__29924__auto___32797,output_channel,vec__32782,success_channel,error_channel){
+var vec__27090 = redditv.reddit.get_subreddit_posts.call(null,subreddit);
+var success_channel = cljs.core.nth.call(null,vec__27090,(0),null);
+var error_channel = cljs.core.nth.call(null,vec__27090,(1),null);
+var c__24744__auto___27105 = cljs.core.async.chan.call(null,(1));
+cljs.core.async.impl.dispatch.run.call(null,((function (c__24744__auto___27105,output_channel,vec__27090,success_channel,error_channel){
 return (function (){
-var f__29925__auto__ = (function (){var switch__29812__auto__ = ((function (c__29924__auto___32797,output_channel,vec__32782,success_channel,error_channel){
-return (function (state_32788){
-var state_val_32789 = (state_32788[(1)]);
-if((state_val_32789 === (1))){
-var state_32788__$1 = state_32788;
-return cljs.core.async.impl.ioc_helpers.take_BANG_.call(null,state_32788__$1,(2),success_channel);
+var f__24745__auto__ = (function (){var switch__24679__auto__ = ((function (c__24744__auto___27105,output_channel,vec__27090,success_channel,error_channel){
+return (function (state_27096){
+var state_val_27097 = (state_27096[(1)]);
+if((state_val_27097 === (1))){
+var state_27096__$1 = state_27096;
+return cljs.core.async.impl.ioc_helpers.take_BANG_.call(null,state_27096__$1,(2),success_channel);
 } else {
-if((state_val_32789 === (2))){
-var inst_32784 = (state_32788[(2)]);
-var inst_32785 = cljs.core.filterv.call(null,redditv.reddit.post_is_video_QMARK_,inst_32784);
-var inst_32786 = cljs.core.async.put_BANG_.call(null,output_channel,inst_32785);
-var state_32788__$1 = state_32788;
-return cljs.core.async.impl.ioc_helpers.return_chan.call(null,state_32788__$1,inst_32786);
+if((state_val_27097 === (2))){
+var inst_27092 = (state_27096[(2)]);
+var inst_27093 = cljs.core.filterv.call(null,redditv.reddit.post_is_video_QMARK_,inst_27092);
+var inst_27094 = cljs.core.async.put_BANG_.call(null,output_channel,inst_27093);
+var state_27096__$1 = state_27096;
+return cljs.core.async.impl.ioc_helpers.return_chan.call(null,state_27096__$1,inst_27094);
 } else {
 return null;
 }
 }
-});})(c__29924__auto___32797,output_channel,vec__32782,success_channel,error_channel))
+});})(c__24744__auto___27105,output_channel,vec__27090,success_channel,error_channel))
 ;
-return ((function (switch__29812__auto__,c__29924__auto___32797,output_channel,vec__32782,success_channel,error_channel){
+return ((function (switch__24679__auto__,c__24744__auto___27105,output_channel,vec__27090,success_channel,error_channel){
 return (function() {
-var redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto__ = null;
-var redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto____0 = (function (){
-var statearr_32793 = [null,null,null,null,null,null,null];
-(statearr_32793[(0)] = redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto__);
+var redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto__ = null;
+var redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto____0 = (function (){
+var statearr_27101 = [null,null,null,null,null,null,null];
+(statearr_27101[(0)] = redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto__);
 
-(statearr_32793[(1)] = (1));
+(statearr_27101[(1)] = (1));
 
-return statearr_32793;
+return statearr_27101;
 });
-var redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto____1 = (function (state_32788){
+var redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto____1 = (function (state_27096){
 while(true){
-var ret_value__29814__auto__ = (function (){try{while(true){
-var result__29815__auto__ = switch__29812__auto__.call(null,state_32788);
-if(cljs.core.keyword_identical_QMARK_.call(null,result__29815__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
+var ret_value__24681__auto__ = (function (){try{while(true){
+var result__24682__auto__ = switch__24679__auto__.call(null,state_27096);
+if(cljs.core.keyword_identical_QMARK_.call(null,result__24682__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
 continue;
 } else {
-return result__29815__auto__;
+return result__24682__auto__;
 }
 break;
 }
-}catch (e32794){if((e32794 instanceof Object)){
-var ex__29816__auto__ = e32794;
-var statearr_32795_32798 = state_32788;
-(statearr_32795_32798[(5)] = ex__29816__auto__);
+}catch (e27102){if((e27102 instanceof Object)){
+var ex__24683__auto__ = e27102;
+var statearr_27103_27106 = state_27096;
+(statearr_27103_27106[(5)] = ex__24683__auto__);
 
 
-cljs.core.async.impl.ioc_helpers.process_exception.call(null,state_32788);
+cljs.core.async.impl.ioc_helpers.process_exception.call(null,state_27096);
 
 return new cljs.core.Keyword(null,"recur","recur",-437573268);
 } else {
-throw e32794;
+throw e27102;
 
 }
 }})();
-if(cljs.core.keyword_identical_QMARK_.call(null,ret_value__29814__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
-var G__32799 = state_32788;
-state_32788 = G__32799;
+if(cljs.core.keyword_identical_QMARK_.call(null,ret_value__24681__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
+var G__27107 = state_27096;
+state_27096 = G__27107;
 continue;
 } else {
-return ret_value__29814__auto__;
+return ret_value__24681__auto__;
 }
 break;
 }
 });
-redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto__ = function(state_32788){
+redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto__ = function(state_27096){
 switch(arguments.length){
 case 0:
-return redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto____0.call(this);
+return redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto____0.call(this);
 case 1:
-return redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto____1.call(this,state_32788);
+return redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto____1.call(this,state_27096);
 }
 throw(new Error('Invalid arity: ' + arguments.length));
 };
-redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto__.cljs$core$IFn$_invoke$arity$0 = redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto____0;
-redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto__.cljs$core$IFn$_invoke$arity$1 = redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto____1;
-return redditv$reddit$get_subreddit_videos_$_state_machine__29813__auto__;
+redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto__.cljs$core$IFn$_invoke$arity$0 = redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto____0;
+redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto__.cljs$core$IFn$_invoke$arity$1 = redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto____1;
+return redditv$reddit$get_subreddit_videos_$_state_machine__24680__auto__;
 })()
-;})(switch__29812__auto__,c__29924__auto___32797,output_channel,vec__32782,success_channel,error_channel))
+;})(switch__24679__auto__,c__24744__auto___27105,output_channel,vec__27090,success_channel,error_channel))
 })();
-var state__29926__auto__ = (function (){var statearr_32796 = f__29925__auto__.call(null);
-(statearr_32796[cljs.core.async.impl.ioc_helpers.USER_START_IDX] = c__29924__auto___32797);
+var state__24746__auto__ = (function (){var statearr_27104 = f__24745__auto__.call(null);
+(statearr_27104[cljs.core.async.impl.ioc_helpers.USER_START_IDX] = c__24744__auto___27105);
 
-return statearr_32796;
+return statearr_27104;
 })();
-return cljs.core.async.impl.ioc_helpers.run_state_machine_wrapped.call(null,state__29926__auto__);
-});})(c__29924__auto___32797,output_channel,vec__32782,success_channel,error_channel))
+return cljs.core.async.impl.ioc_helpers.run_state_machine_wrapped.call(null,state__24746__auto__);
+});})(c__24744__auto___27105,output_channel,vec__27090,success_channel,error_channel))
 );
 
 
