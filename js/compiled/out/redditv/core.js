ad187bdfc1b94af344380804ab6fb9cd74d8459f
Benjamin Zaporzan <benzaporzan@gmail.com>
2017-03-20 19:21:13 -0400

    updated production site

--- js/compiled/out/redditv/core.js
+++ js/compiled/out/redditv/core.js
@@ -35,29 +35,28 @@ if(typeof redditv.core.fullscreen !== 'undefined'){
 } else {
 redditv.core.fullscreen = rum.core.cursor_in.call(null,redditv.core.app_state,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"fullscreen","fullscreen",-4371054)], null));
 }
-redditv.playlist.reload.call(null,redditv.core.app_state);
 secretary.core.set_config_BANG_.call(null,new cljs.core.Keyword(null,"prefix","prefix",-265908465),"#");
 /**
  * Set the location hash of a js/window object.
  */
 redditv.core.set_hash_BANG_ = (function redditv$core$set_hash_BANG_(var_args){
-var args38020 = [];
-var len__31204__auto___38023 = arguments.length;
-var i__31205__auto___38024 = (0);
+var args37691 = [];
+var len__31204__auto___37694 = arguments.length;
+var i__31205__auto___37695 = (0);
 while(true){
-if((i__31205__auto___38024 < len__31204__auto___38023)){
-args38020.push((arguments[i__31205__auto___38024]));
+if((i__31205__auto___37695 < len__31204__auto___37694)){
+args37691.push((arguments[i__31205__auto___37695]));
 
-var G__38025 = (i__31205__auto___38024 + (1));
-i__31205__auto___38024 = G__38025;
+var G__37696 = (i__31205__auto___37695 + (1));
+i__31205__auto___37695 = G__37696;
 continue;
 } else {
 }
 break;
 }
 
-var G__38022 = args38020.length;
-switch (G__38022) {
+var G__37693 = args37691.length;
+switch (G__37693) {
 case 1:
 return redditv.core.set_hash_BANG_.cljs$core$IFn$_invoke$arity$1((arguments[(0)]));
 
@@ -67,7 +66,7 @@ return redditv.core.set_hash_BANG_.cljs$core$IFn$_invoke$arity$2((arguments[(0)]
 
 break;
 default:
-throw (new Error([cljs.core.str("Invalid arity: "),cljs.core.str(args38020.length)].join('')));
+throw (new Error([cljs.core.str("Invalid arity: "),cljs.core.str(args37691.length)].join('')));
 
 }
 });
@@ -82,35 +81,35 @@ return (loc["hash"] = v);
 
 redditv.core.set_hash_BANG_.cljs$lang$maxFixedArity = 2;
 
-var action__37855__auto___38033 = (function (params__37856__auto__){
-if(cljs.core.map_QMARK_.call(null,params__37856__auto__)){
-var map__38027 = params__37856__auto__;
-var map__38027__$1 = ((((!((map__38027 == null)))?((((map__38027.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__38027.cljs$core$ISeq$)))?true:false):false))?cljs.core.apply.call(null,cljs.core.hash_map,map__38027):map__38027);
-var subreddit = cljs.core.get.call(null,map__38027__$1,new cljs.core.Keyword(null,"subreddit","subreddit",-670654754));
+var action__37526__auto___37704 = (function (params__37527__auto__){
+if(cljs.core.map_QMARK_.call(null,params__37527__auto__)){
+var map__37698 = params__37527__auto__;
+var map__37698__$1 = ((((!((map__37698 == null)))?((((map__37698.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__37698.cljs$core$ISeq$)))?true:false):false))?cljs.core.apply.call(null,cljs.core.hash_map,map__37698):map__37698);
+var subreddit = cljs.core.get.call(null,map__37698__$1,new cljs.core.Keyword(null,"subreddit","subreddit",-670654754));
 return cljs.core.swap_BANG_.call(null,redditv.core.app_state,cljs.core.assoc,new cljs.core.Keyword(null,"subreddit","subreddit",-670654754),subreddit);
 } else {
-if(cljs.core.vector_QMARK_.call(null,params__37856__auto__)){
-var vec__38029 = params__37856__auto__;
-var subreddit = cljs.core.nth.call(null,vec__38029,(0),null);
+if(cljs.core.vector_QMARK_.call(null,params__37527__auto__)){
+var vec__37700 = params__37527__auto__;
+var subreddit = cljs.core.nth.call(null,vec__37700,(0),null);
 return cljs.core.swap_BANG_.call(null,redditv.core.app_state,cljs.core.assoc,new cljs.core.Keyword(null,"subreddit","subreddit",-670654754),subreddit);
 } else {
 return null;
 }
 }
 });
-secretary.core.add_route_BANG_.call(null,"/r/:subreddit",action__37855__auto___38033);
+secretary.core.add_route_BANG_.call(null,"/r/:subreddit",action__37526__auto___37704);
 
-redditv.core.subreddit_path = ((function (action__37855__auto___38033){
+redditv.core.subreddit_path = ((function (action__37526__auto___37704){
 return (function redditv$core$subreddit_path(var_args){
 var args__31211__auto__ = [];
-var len__31204__auto___38034 = arguments.length;
-var i__31205__auto___38035 = (0);
+var len__31204__auto___37705 = arguments.length;
+var i__31205__auto___37706 = (0);
 while(true){
-if((i__31205__auto___38035 < len__31204__auto___38034)){
-args__31211__auto__.push((arguments[i__31205__auto___38035]));
+if((i__31205__auto___37706 < len__31204__auto___37705)){
+args__31211__auto__.push((arguments[i__31205__auto___37706]));
 
-var G__38036 = (i__31205__auto___38035 + (1));
-i__31205__auto___38035 = G__38036;
+var G__37707 = (i__31205__auto___37706 + (1));
+i__31205__auto___37706 = G__37707;
 continue;
 } else {
 }
@@ -119,38 +118,38 @@ break;
 
 var argseq__31212__auto__ = ((((0) < args__31211__auto__.length))?(new cljs.core.IndexedSeq(args__31211__auto__.slice((0)),(0),null)):null);
 return redditv.core.subreddit_path.cljs$core$IFn$_invoke$arity$variadic(argseq__31212__auto__);
-});})(action__37855__auto___38033))
+});})(action__37526__auto___37704))
 ;
 
-redditv.core.subreddit_path.cljs$core$IFn$_invoke$arity$variadic = ((function (action__37855__auto___38033){
-return (function (args__37854__auto__){
-return cljs.core.apply.call(null,secretary.core.render_route_STAR_,"/r/:subreddit",args__37854__auto__);
-});})(action__37855__auto___38033))
+redditv.core.subreddit_path.cljs$core$IFn$_invoke$arity$variadic = ((function (action__37526__auto___37704){
+return (function (args__37525__auto__){
+return cljs.core.apply.call(null,secretary.core.render_route_STAR_,"/r/:subreddit",args__37525__auto__);
+});})(action__37526__auto___37704))
 ;
 
 redditv.core.subreddit_path.cljs$lang$maxFixedArity = (0);
 
-redditv.core.subreddit_path.cljs$lang$applyTo = ((function (action__37855__auto___38033){
-return (function (seq38032){
-return redditv.core.subreddit_path.cljs$core$IFn$_invoke$arity$variadic(cljs.core.seq.call(null,seq38032));
-});})(action__37855__auto___38033))
+redditv.core.subreddit_path.cljs$lang$applyTo = ((function (action__37526__auto___37704){
+return (function (seq37703){
+return redditv.core.subreddit_path.cljs$core$IFn$_invoke$arity$variadic(cljs.core.seq.call(null,seq37703));
+});})(action__37526__auto___37704))
 ;
 
-var h_38039 = (new goog.History());
-goog.events.listen(h_38039,goog.history.EventType.NAVIGATE,((function (h_38039){
-return (function (p1__38037_SHARP_){
-return secretary.core.dispatch_BANG_.call(null,p1__38037_SHARP_.token);
-});})(h_38039))
+var h_37710 = (new goog.History());
+goog.events.listen(h_37710,goog.history.EventType.NAVIGATE,((function (h_37710){
+return (function (p1__37708_SHARP_){
+return secretary.core.dispatch_BANG_.call(null,p1__37708_SHARP_.token);
+});})(h_37710))
 );
 
-var G__38038_38040 = h_38039;
-G__38038_38040.setEnabled(true);
+var G__37709_37711 = h_37710;
+G__37709_37711.setEnabled(true);
 
 redditv.core.mixin_keyboard_controls = new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"did-mount","did-mount",918232960),(function (state){
 return window.addEventListener("keydown",(function (e){
 var keycode = e.keyCode;
-var G__38041 = keycode;
-switch (G__38041) {
+var G__37712 = keycode;
+switch (G__37712) {
 case (37):
 return redditv.playlist.select_prev.call(null,redditv.core.app_state);
 
@@ -166,7 +165,8 @@ return null;
 }));
 })], null);
 redditv.core.app = rum.core.build_defc.call(null,(function (){
-var attrs38043 = redditv.components.header.c_header.call(null,redditv.core.app_state);
-return cljs.core.apply.call(null,React.createElement,"div",((cljs.core.map_QMARK_.call(null,attrs38043))?sablono.interpreter.attributes.call(null,sablono.normalize.merge_with_class.call(null,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, ["redditv-main"], null)], null),attrs38043)):({"className": "redditv-main"})),((cljs.core.map_QMARK_.call(null,attrs38043))?new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [sablono.interpreter.interpret.call(null,redditv.components.sidepane.c_sidepane.call(null,redditv.core.app_state)),sablono.interpreter.interpret.call(null,redditv.components.player.c_player.call(null,redditv.core.app_state,redditv.core.playlist_index,redditv.core.show_playlist,redditv.core.fullscreen)),sablono.interpreter.interpret.call(null,redditv.components.playlist.c_playlist.call(null,redditv.core.app_state))], null):new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [sablono.interpreter.interpret.call(null,attrs38043),sablono.interpreter.interpret.call(null,redditv.components.sidepane.c_sidepane.call(null,redditv.core.app_state)),sablono.interpreter.interpret.call(null,redditv.components.player.c_player.call(null,redditv.core.app_state,redditv.core.playlist_index,redditv.core.show_playlist,redditv.core.fullscreen)),sablono.interpreter.interpret.call(null,redditv.components.playlist.c_playlist.call(null,redditv.core.app_state))], null)));
+var attrs37714 = redditv.components.header.c_header.call(null,redditv.core.app_state);
+return cljs.core.apply.call(null,React.createElement,"div",((cljs.core.map_QMARK_.call(null,attrs37714))?sablono.interpreter.attributes.call(null,sablono.normalize.merge_with_class.call(null,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, ["redditv-main"], null)], null),attrs37714)):({"className": "redditv-main"})),((cljs.core.map_QMARK_.call(null,attrs37714))?new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [sablono.interpreter.interpret.call(null,redditv.components.sidepane.c_sidepane.call(null,redditv.core.app_state)),sablono.interpreter.interpret.call(null,redditv.components.player.c_player.call(null,redditv.core.app_state,redditv.core.playlist_index,redditv.core.show_playlist,redditv.core.fullscreen)),sablono.interpreter.interpret.call(null,redditv.components.playlist.c_playlist.call(null,redditv.core.app_state))], null):new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [sablono.interpreter.interpret.call(null,attrs37714),sablono.interpreter.interpret.call(null,redditv.components.sidepane.c_sidepane.call(null,redditv.core.app_state)),sablono.interpreter.interpret.call(null,redditv.components.player.c_player.call(null,redditv.core.app_state,redditv.core.playlist_index,redditv.core.show_playlist,redditv.core.fullscreen)),sablono.interpreter.interpret.call(null,redditv.components.playlist.c_playlist.call(null,redditv.core.app_state))], null)));
 }),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [redditv.core.mixin_keyboard_controls], null),"app");
+redditv.playlist.reload.call(null,redditv.core.app_state);
 rum.core.mount.call(null,redditv.core.app.call(null),document.querySelector("#app"));
